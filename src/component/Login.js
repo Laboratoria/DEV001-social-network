53cd5d587915254baa52fe3a4d31031f72737c26
Adriana Araujo <araujovillasmil@gmail.com>
2022-11-08 18:35:32 -0300

    Cambios inicio sesion css

--- src/component/Login.js
+++ src/component/Login.js
@@ -4,12 +4,13 @@ import { formulariologin } from '../lib/index.js';
 
 export const Login = () => {
   const HomeDiv = document.createElement('div');
-  HomeDiv.textContent = 'Bienvenido al Login';
-  const buttonHome = document.createElement('button');
-  buttonHome.textContent = 'Regresar al home';
-  buttonHome.addEventListener('click', () => onNavigate('/'));
-  HomeDiv.appendChild(buttonHome);
-
+  HomeDiv.className = 'divpadre2';
+  const divBienvenida2 = document.createElement('div');
+  HomeDiv.appendChild(divBienvenida2);
+  const mensajeBienvenida2 = document.createElement('h2');
+  mensajeBienvenida2.textContent = 'Welcome to lymusic';
+  divBienvenida2.appendChild('mensajeBienvenida2');
+  divBienvenida2.className = 'mensajeBienvenida2';
   const form = document.createElement('form');
   const divForm = document.createElement('div');
   HomeDiv.appendChild(form);
@@ -17,14 +18,17 @@ export const Login = () => {
   const tituloForm = document.createElement('h1');
   tituloForm.textContent = 'Inicia sesión';
   divForm.appendChild(tituloForm);
+  divForm.className = 'formulariologin';
   const grupo1 = document.createElement('div');
   const grupo2 = document.createElement('div');
   const email = document.createElement('input');
   email.type = 'email';
+  email.placeholder = 'Email';
   const labelEmail = document.createElement('label');
   labelEmail.textContent = 'Email';
   const pass = document.createElement('input');
   pass.type = 'password';
+  pass.placeholder = 'Password';
   const labelPass = document.createElement('label');
   labelPass.textContent = 'Password';
   const buttonSubmit = document.createElement('button');
@@ -36,10 +40,19 @@ export const Login = () => {
   divForm.appendChild(grupo1);
   divForm.appendChild(grupo2);
   divForm.appendChild(buttonSubmit);
+  grupo2.className = 'grupos';
+  grupo1.className = 'grupos';
   buttonSubmit.type = 'submit';
   form.addEventListener('submit', (e) => {
     e.preventDefault();
     formulariologin(email.value, pass.value);
   });
+  const divBoton = document.createElement('div');
+  divBoton.className = 'botonRegreso2';
+  HomeDiv.appendChild(divBoton);
+  const buttonHome = document.createElement('button');
+  buttonHome.textContent = 'Back to home';
+  divBoton.appendChild(buttonHome);
+  buttonHome.addEventListener('click', () => onNavigate('/'));
   return HomeDiv;
 };
