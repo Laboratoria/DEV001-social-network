807765cf122a4a14e10e5dba84bfffc6a2266a07
gabyzam <ruth.gabriela.pz@gmail.com>
2022-11-09 10:12:37 -0500

    Añadimos estilos al home de nuestra APP

--- src/lib/Archivos JS/home.js
+++ src/lib/Archivos JS/home.js
@@ -1,19 +1,46 @@
 // eslint-disable-next-line import/no-cycle
-import { onNavigate } from '../../main';
+import { onNavigate } from "../../main";
+
+const rootDiv = document.getElementById('root');
 
 export const home = () => {
-  const homeDiv = document.createElement('div');
-  const buttonRegister = document.createElement('button');
-  const buttonLogin = document.createElement('button');
+  rootDiv.innerHTML = " ";
 
-  buttonRegister.textContent = 'Registrarme';
-  buttonLogin.textContent = 'Iniciar sesión';
+  const homeDiv = document.createElement("div");
+  const containerElements = document.createElement("section");
+  const imgLogo = document.createElement("img");
+  const description = document.createElement("p");
+  const buttonRegister = document.createElement("button");
+  const buttonLogin = document.createElement("button");
+  const imgTop = document.createElement("img");
+  const imgBottom = document.createElement("img");
 
-  buttonRegister.addEventListener('click', () => onNavigate('/register'));
-  buttonLogin.addEventListener('click', () => onNavigate('/login'));
+  homeDiv.className = "container"
+  buttonRegister.textContent = "Regístrate";
+  buttonRegister.className = "buttonRegister";
+  buttonLogin.textContent = "Iniciar sesión";
+  buttonLogin.className = "buttonLogin";
+  containerElements.className = "container-home"
+  imgLogo.src = "./lib/img/logo-24-7.png";
+  imgLogo.className = "logo-24-7"
+  imgTop.src = "./lib/img/collage-1.png"
+  imgTop.className = "img-top"
+  imgBottom.src = "./lib/img/collage-5.png"
+  imgBottom.className = "img-bottom"
+  description.textContent =
+    '24/7 es una APP diseñada con el objetivo de crear un espacio de interacción para mujeres.';
+  description.className = "text-description";
 
-  homeDiv.appendChild(buttonRegister);
-  homeDiv.appendChild(buttonLogin);
+  buttonRegister.addEventListener("click", () => onNavigate("/register"));
+  buttonLogin.addEventListener("click", () => onNavigate("/login"));
+
+  homeDiv.appendChild(containerElements);
+  containerElements.appendChild(imgLogo);
+  containerElements.appendChild(description);
+  containerElements.appendChild(buttonRegister);
+  containerElements.appendChild(buttonLogin);
+  homeDiv.appendChild(imgTop);
+  homeDiv.appendChild(imgBottom);
 
   return homeDiv;
 };
