df5a6c59b76a71034f900d66a6eea553120655ef
Valeria Murguia <valeria.murguia.g@gmail.com>
2022-11-10 13:18:56 -0600

    Autenticación con Google lista, día 3

--- src/lib/Archivos JS/register.js
+++ src/lib/Archivos JS/register.js
@@ -1,103 +1,98 @@
-import { onNavigate } from "../../main";
-import { createUserWithEmailAndPassword } from "firebase/auth";
-import { onAuthStateChanged } from "firebase/auth";
-import { auth } from "./firebase.js";
+import {
+  createUserWithEmailAndPassword, GoogleAuthProvider, signInWithPopup, onAuthStateChanged,
+} from 'firebase/auth';
+import { auth } from './firebase.js';
+// eslint-disable-next-line import/no-cycle
+import { onNavigate } from '../../main';
 
 onAuthStateChanged(auth, async (user) => {
   if (user) {
-
-  } else {
-
+    console.log(user);
   }
 });
 
-
-const rootDiv = document.getElementById("root");
+const rootDiv = document.getElementById('root');
 
 export const register = () => {
-  rootDiv.innerHTML = " ";
+  rootDiv.innerHTML = ' ';
 
-  const homeDiv = document.createElement("div");
-  const container = document.createElement("section")
-  const containerRegister = document.createElement("div");
-  const containerImg = document.createElement("section")
-  const imgwelcome = document.createElement("img");
-  const imgTop = document.createElement("img");
-  const imgBottom = document.createElement("img");
-  const title = document.createElement("h2")
-  const titleImg = document.createElement("img");
-  const homeDiv2 = document.createElement("div");
-  const p = document.createElement("p");
-  const inputName = document.createElement("input");
-  const homeDiv3 = document.createElement("div");
-  const p2 = document.createElement("p");
-  const inputEmail = document.createElement("input");
-  const homeDiv4 = document.createElement("div");
-  const p3 = document.createElement("p");
-  const inputPassword = document.createElement("input");
-  const homeDiv5 = document.createElement("div");
-  const buttonGoogle = document.createElement("button");
-  const buttonSend = document.createElement("button");
-  const imgFlower = document.createElement("img");
-  const imgFlower2 = document.createElement("img");
-  const imgFlower3 = document.createElement("img");
-  const regresar = document.createElement("p");
+  const homeDiv = document.createElement('div');
+  const container = document.createElement('section');
+  const containerRegister = document.createElement('div');
+  const containerImg = document.createElement('section');
+  const imgwelcome = document.createElement('img');
+  const imgTop = document.createElement('img');
+  const imgBottom = document.createElement('img');
+  const title = document.createElement('h2');
+  const titleImg = document.createElement('img');
+  const homeDiv2 = document.createElement('div');
+  const p = document.createElement('p');
+  const inputName = document.createElement('input');
+  const homeDiv3 = document.createElement('div');
+  const p2 = document.createElement('p');
+  const inputEmail = document.createElement('input');
+  const homeDiv4 = document.createElement('div');
+  const p3 = document.createElement('p');
+  const inputPassword = document.createElement('input');
+  const homeDiv5 = document.createElement('div');
+  const buttonGoogle = document.createElement('button');
+  const buttonSend = document.createElement('button');
+  const imgFlower = document.createElement('img');
+  const imgFlower2 = document.createElement('img');
+  const imgFlower3 = document.createElement('img');
+  const regresar = document.createElement('p');
+  homeDiv.className = 'container';
+  container.className = 'container-im-and-register';
+  containerImg.className = 'container-img';
+  titleImg.src = './lib/img/flowers1.png';
+  titleImg.className = 'img-title';
+  title.textContent = 'Regístrate';
+  title.className = 'title-register';
+  imgwelcome.src = './lib/img/img-women.png';
+  imgwelcome.className = 'img-welcome';
+  imgTop.src = './lib/img/collage-3.png';
+  imgTop.className = 'img-top-2';
+  imgBottom.src = './lib/img/collage-5.png';
+  imgBottom.className = 'img-bottom-2';
+  containerRegister.className = 'container-register';
+  p.textContent = 'Nombre completo';
+  p.className = 'text-subtitle';
+  inputName.type = 'text';
+  inputName.id = 'inputName';
+  inputName.className = 'input-register';
+  inputName.required = 'true';
+  inputName.placeholder = 'Ingresa tu nombre:';
+  p2.textContent = 'Correo electrónico';
+  p2.className = 'text-subtitle';
+  inputEmail.type = 'email';
+  inputEmail.id = 'inputEmail';
+  inputEmail.className = 'input-register';
+  inputEmail.required = 'true';
+  inputEmail.placeholder = 'Ingresa tu email:';
+  p3.textContent = 'Contraseña';
+  p3.className = 'text-subtitle';
+  inputPassword.type = 'password';
+  inputPassword.id = 'inputPassword';
+  inputPassword.className = 'input-register';
+  inputPassword.required = 'true';
+  inputPassword.placeholder = 'Ingresa tu contraseña:';
+  regresar.innerHTML = '¿Ya tienes cuenta? <a href> Iniciar sesión </a>';
+  regresar.className = 'link-login';
+  buttonGoogle.innerHTML = '<img class="img-google" src="https://icones.pro/wp-content/uploads/2021/02/google-icone-symbole-logo-png-150x150.png" /> Regístrarme con google';
+  buttonGoogle.className = 'button-google';
+  buttonSend.textContent = 'Registrarme';
+  buttonSend.className = 'buttonRegister';
+  imgFlower.src = './lib/img/flowers2.png';
+  imgFlower.className = 'icon-img-flower';
+  imgFlower2.src = './lib/img/flowers2.png';
+  imgFlower2.className = 'icon-img-flower';
+  imgFlower3.src = './lib/img/flowers2.png';
+  imgFlower3.className = 'icon-img-flower';
+  homeDiv2.className = 'container-div';
+  homeDiv3.className = 'container-div';
+  homeDiv4.className = 'container-div';
+  homeDiv5.className = 'container-div';
 
-
-  
-  homeDiv.className = "container";
-  container.className = "container-im-and-register"
-  containerImg.className = "container-img";
-  titleImg.src = "./lib/img/flowers1.png";
-  titleImg.className = "img-title";
-  title.textContent = "Regístrate";
-  title.className = "title-register";
-  imgwelcome.src = "./lib/img/img-women.png";
-  imgwelcome.className = "img-welcome";
-  imgTop.src = "./lib/img/collage-3.png";
-  imgTop.className = "img-top-2";
-  imgBottom.src = "./lib/img/collage-5.png"
-  imgBottom.className = "img-bottom-2"
-  containerRegister.className = "container-register"
-  p.textContent = "Nombre completo";
-  p.className = "text-subtitle";
-  inputName.type = "text";
-  inputName.id = "inputName";
-  inputName.className = "input-register";
-  inputName.required = "true";
-  inputName.placeholder = "Ingresa tu nombre:";
-  p2.textContent = "Correo electrónico";
-  p2.className = "text-subtitle";
-  inputEmail.type = "email";
-  inputEmail.id = "inputEmail";
-  inputEmail.className = "input-register";
-  inputEmail.required = "true";
-  inputEmail.placeholder = "Ingresa tu email:";
-  p3.textContent = "Contraseña";
-  p3.className = "text-subtitle";
-  inputPassword.type = "password";
-  inputPassword.id = "inputPassword";
-  inputPassword.className = "input-register";
-  inputPassword.required = "true";
-  inputPassword.placeholder = "Ingresa tu contraseña:";
-  regresar.innerHTML = "¿Ya tienes cuenta? <a href> Iniciar sesión </a>";
-  regresar.className = "link-login";
-  buttonGoogle.innerHTML = `<img class="img-google" src="https://icones.pro/wp-content/uploads/2021/02/google-icone-symbole-logo-png-150x150.png" /> Regístrarme con google`;
-  buttonGoogle.className = "button-google";
-  buttonSend.textContent = "Registrarme";
-  buttonSend.className = "buttonRegister";
-  imgFlower.src = "./lib/img/flowers2.png";
-  imgFlower.className = "icon-img-flower";
-  imgFlower2.src = "./lib/img/flowers2.png";
-  imgFlower2.className = "icon-img-flower";
-  imgFlower3.src = "./lib/img/flowers2.png";
-  imgFlower3.className = "icon-img-flower";
-  homeDiv2.className = "container-div";
-  homeDiv3.className = "container-div";
-  homeDiv4.className = "container-div";
-  homeDiv5.className = "container-div";
-
-  
   homeDiv.appendChild(container);
   container.appendChild(containerImg);
   container.appendChild(containerRegister);
@@ -122,40 +117,46 @@ export const register = () => {
   homeDiv5.appendChild(buttonGoogle);
   homeDiv5.appendChild(regresar);
   containerRegister.appendChild(homeDiv5);
-  
-  
-  regresar.addEventListener("click", () => onNavigate("/login"));
-  
-
-  buttonSend.addEventListener("click", async () => {
+  regresar.addEventListener('click', () => onNavigate('/login'));
+  buttonSend.addEventListener('click', async () => {
     const name = inputName.value;
     const email = inputEmail.value;
     const password = inputPassword.value;
 
     console.log(name, email, password);
-
     try {
       const userCredentials = await createUserWithEmailAndPassword(
         auth,
         email,
-        password
+        password,
       );
       console.log(userCredentials);
 
-      onNavigate("/profile");
+      onNavigate('/profile');
     } catch (error) {
       console.log(error);
-      if (error.code === "auth/email-already-in-use") {
-        alert("El correo ya está registrado");
-      } else if (error.code === "auth/invalid-email") {
-        alert("Debes ingresar un correo válido");
-      } else if (error.code === "auth/weak-password") {
-        alert("La contraseña debe tener al menos 6 carácteres");
+      if (error.code === 'auth/email-already-in-use') {
+        alert('El correo ya está registrado');
+      } else if (error.code === 'auth/invalid-email') {
+        alert('Debes ingresar un correo válido');
+      } else if (error.code === 'auth/weak-password') {
+        alert('La contraseña debe tener al menos 6 carácteres');
       } else if (error.code) {
-        alert("Algo está mal en tu registro");
+        alert('Algo está mal en tu registro');
       }
       // const errorCode = error.code;
       // const errorMessage = error.message;
+    }
+  });
+
+  buttonGoogle.addEventListener('click', async () => {
+    const provider = new GoogleAuthProvider();
+    try {
+      const credentials = await signInWithPopup(auth, provider);
+      console.log(credentials);
+      onNavigate('/profile');
+    } catch (error) {
+      console.log(error);
     }
   });
 
