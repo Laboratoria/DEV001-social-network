451c7a3a3c40922f3d3e6ef688d1aca29a2628d8
Lorenaguillen <guillen.lorena88@gmail.com>
2022-11-09 19:44:04 -0500

    validando formulario prueba 1

--- src/lib/validar.js
+++ src/lib/validar.js
@@ -1,6 +1,26 @@
 import { formularioregistro } from './index.js';
 
-export const validar = (email, password) => {
+export const validar = (name, email, password) => {
   // aqui se valida el formulario
-  formularioregistro(email, password);
+  formularioregistro(name, email, password);
+  //console.log('validar' + name, email, password);
+  let entrar = false
+  let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+  if (name.length < 6) {
+    alert('El nombre no es válido');
+  }
+  console.log(regexEmail.test(email))
+  if (!regexEmail.test(email)) {
+    alert('El email no es válido');
+    entrar = true
+  }
+  if (password.length < 8) {
+    alert('La constraseña no es válida');
+    entrar = true
+  }
+  // if (entrar) {
+
+  // }
 };
+
+
