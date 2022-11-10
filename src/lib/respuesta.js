f8f3a9c85031c3ce5009d1187273c10e7cea3189
camila leal <khammylv@gmail.com>
2022-11-10 12:27:31 -0500

    validacion

--- src/lib/respuesta.js
+++ src/lib/respuesta.js
@@ -1,10 +1,2 @@
 // eslint-disable-next-line no-shadow
-export const respuesta = (respuesta) => {
-  // eslint-disable-next-line spaced-comment
-  //aqui se obtiene la respuesta del firebase
-  if (respuesta) {
-    alert(`si es  ${respuesta}`);
-  } else {
-    alert(`else ${respuesta}`);
-  }
-};
+export const respuesta = (respuesta) => respuesta;
