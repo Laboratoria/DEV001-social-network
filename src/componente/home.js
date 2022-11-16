5180be1540615ea6cd5592db566c24b768be1cbf
Gabriela Wongchuig <gaba@MacBook-Air-de-Gabriela.local>
2022-11-16 14:03:14 -0500

    actualizacion de css

--- src/componente/home.js
+++ src/componente/home.js
@@ -1,11 +1,15 @@
+// import { onNavigate } from "../main.js";
+
 export const Home = () => {
-  const HomeDiv = document.createElement('div');
+  const HomeDiv = document.createElement('section');
   HomeDiv.innerHTML = `
   <article class = "plantilla">
-   <div><img class="logo" src="img/logo.png" alt=""></div>
-   <div><img class="niña" src="img/una.png" alt=""></div>
+   <div class= "imagenes">
+   <img class="logo" src="img/logo.png" alt="">
+   <img class="girl" src="img/una.png" alt="">
+   </div>
    <div>
-    <input type = "text" class= "usuario" placeholder = "Usuario o correo electrónico">
+    <input type = "text" class= "usuario" placeholder = "Usuario o correo electrónico"><br>
     <input type = "text" class= "usuario" placeholder = "Contraseña">
   </div>
   </article>
@@ -14,9 +18,11 @@ export const Home = () => {
   const buttonLogin = document.createElement('button');
 
   buttonLogin.textContent = 'Iniciar sesión';
-
   HomeDiv.appendChild(buttonLogin);
+
+  // buttonLogin.addEventListener('click', () => onNavigate('/muro'));
+
+
 
   return HomeDiv;
 };
-console.log(Home());
